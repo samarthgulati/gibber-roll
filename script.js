7e18e9712c714402b0501e0e3e52e024b57f0636
Abhinay Khoparzi <abhinayk@gmail.com>
2020-10-09 16:42:34 +0530

    Hacky solution for keeping the BPM constant when number of steps are changed. But it works!

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ function buildKeyboard(min, max) {
 
 var columns = 16;
 var rows = 16;
+var bpmMult = 1;
 
 var incBy = 1;
 
@@ -260,7 +261,7 @@ function clearGrid() {
 }
 
 function updateBpm() {
-  Clock.bpm = bpmEl.value;
+  Clock.bpm = bpmEl.value * bpmMult;
 }
 
 function updateBpmInput() {
@@ -486,6 +487,8 @@ function updateColumns() {
     }
   }
   columns = columnsEl.value;
+  bpmMult = (16 / columnsEl.value);
+  Clock.bpm = bpmEl.value * bpmMult;
   if(isPlaying) {
     play();
   }
@@ -529,7 +532,7 @@ function getCode() {
   updateSequence();
   var preset = presetEl.value === '' ? undefined : presetEl.value;
   return `
-Clock.bpm = ${bpmEl.value};
+Clock.bpm = ${bpmEl.value * bpmMult};
 reverb = Bus2().fx.add( Freeverb() );
 delayFX = Delay({ time: 1 / 6, feedback: .75 });
 delay = Bus2().fx.add( delayFX );
