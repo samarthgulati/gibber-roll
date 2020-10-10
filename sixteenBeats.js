cf500b19aedcc7804c29336545f1b590ca5a43d9
Abhinay Khoparzi <abhinayk@gmail.com>
2020-10-10 10:03:52 +0530

    A collection of basic beats
    Beats suggested by Drum Beats Online in this video https://www.youtube.com/watch?v=mwe4HPOiUms

---
+++ sixteenBeats.js
@@ -0,0 +1,72 @@
+// A collection of basic beats suggested by Drum Beats Online in this video
+// https://www.youtube.com/watch?v=mwe4HPOiUms
+// For a more complete version with TidalCycles, check out
+// https://gist.github.com/khoparzi/622b7892a95444a2ee9930d432561985
+
+module.exports = {
+    fourOnFloor: {
+        steps: { "ch": "x.x.x.x.x.x.x.x.", "sd": "....x.......x...", "kd": "x.......x......." },
+        bpmMult: 1
+    },
+    dancebeat: {
+        steps: { "ch": "..x...x...x...x.", "sd": "....x.......x...", "kd": "x.......x......." },
+        bpmMult: 1
+    },
+    firstbeat: {
+        steps: { "ch": "x.x.x.x.x.x.x.x.", "sd": "....x.......x...", "kd": "x.......x.x....." },
+        bpmMult: 1
+    },
+    secondbeat: {
+        steps: { "ch": "x.x.x.x.x.x.x.x.", "sd": "....x.......x...", "kd": "x.x.....x.x....." },
+        bpmMult: 1
+    },
+    classicRock: {
+        steps: { "ch": "x.x.x.x.x.x.x.x.", "sd": "....x.......x...", "kd": "x.x...x.x.x...x." },
+        bpmMult: 1
+    },
+    classicRock2: {
+        steps: { "ch": "x.x.x.x.x.x.x.x.", "sd": "x...x...x...x...", "kd": "..........x...x." },
+        bpmMult: 1
+    },
+    classicHalfTime: {
+        steps: { "oh": "x...x...x...x...", "sd": "........x.......", "kd": "x..x..x........." },
+        bpmMult: 1
+    },
+    classicHalfTime16s: {
+        steps: { "ch": "xxxxxxxxxxxxxxxx", "sd": "........x.......", "kd": "x..x..x........." },
+        bpmMult: 1
+    },
+    classicHalfTimeAtNormal: {
+        steps: { "ch": "x.x.x.x.x.x.x.x.", "sd": "....x.......x...", "kd": "x..x..x........." },
+        bpmMult: 1
+    },
+    changingItUp: {
+        steps: { "oh": "x...x...x...x...", "sd": "x.....x.....x...", "kd": "..x.x...x.x...x." },
+        bpmMult: 1
+    },
+    bieberBeat: {
+        steps: { "ch": "x.x.x.x.x.x.x.x.", "sd": "....x..x.x..x...", "kd": "x.x.......x....." },
+        bpmMult: 1
+    },
+    popCountryBeat: {
+        steps: { "ch": "x.x.x.x.x.x.x.x.", "sd": "....x.......x...", "kd": "x.........x....." },
+        bpmMult: 1
+    },
+    doubleTimeBeat: {
+        steps: { "ch": "x.x.x.x.x.x.x.x.", "sd": "..x...x...x...x.", "kd": "x...xx..x...xx.." },
+        bpmMult: 1
+    },
+    jungleBeat: {
+        steps: { "sd": "...x..x..x..x...", "kd": "x...x...x...x..." },
+        bpmMult: 1
+    },
+    crossStickBeat: {
+        steps: { "ch": "x.x.x.x.x.x.x.x.", "sd": "........x..x..x.", "kd": "x...x..........." },
+        bpmMult: 1
+    },
+    fourOnFloorHalfTime: {
+        steps: { "oh": "x...x...x...x...", "sd": "........x.......", "kd": "x...x...x...x..." },
+        bpmMult: 1
+    },
+
+}
